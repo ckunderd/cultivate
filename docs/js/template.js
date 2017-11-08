1ffd2637a8f0791fa1d3abd151e5e15d882714b2
Cameron Underdown <New@sardMBP.local>
2017-11-07 19:08:37 -0500

    Rotating Header Gallery

--- docs/js/template.js
+++ docs/js/template.js
@@ -36,3 +36,43 @@ jQuery(document).ready(function($) {
 	});
 
 });
+
+/*! slides | https://gist.github.com/mhulse/66bcbb7099bb4beae530 
+(function($) {
+	
+	'use strict';
+	
+	var $slides = $('[data-slides]');
+	var images = $slides.data('slides');
+	var count = images.length;
+	var slideshow = function() {
+		$slides
+			.css('background-image', 'url("' + images[Math.floor(Math.random() * count)] + '")')
+			.show(0, function() {
+				setTimeout(slideshow, 5000);
+			});
+	};
+	
+	slideshow();
+	
+}(jQuery));*/
+/*!
+ * By Eharry.me (https://gist.github.com/Ema4rl/b8ef90be99205ddada5ef2cd6e632ebe)
+ */
+ ! function ($) {
+    "use strict";
+    var slide = $("[data-slides]"),
+        count = 0,
+        slides = slide.data("slides"),
+        len = slides.length,
+        n = function () {
+            if (count >= len) {
+                count = 0
+            }
+            slide.css("background-image", 'url("' + slides[count] + '")').show(0, function () {
+                setTimeout(n, 5e3)
+            });
+            count++;
+        };
+    n()
+}(jQuery);
